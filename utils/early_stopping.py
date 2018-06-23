cd3b350bdf86fd9c033e16d2ae71ab085b9aeee6
Katerina Margatina <katerina.margatina@gmail.com>
2018-06-23 18:58:08 +0300

    early stopping implementation also for mode="min".

--- utils/early_stopping.py
+++ utils/early_stopping.py
@@ -9,7 +9,10 @@ class Early_stopping():
         self.mode = mode
         self.patience = patience
         self.current_patience = patience
-        self.best_metric = 0.0
+        if self.mode == "max":
+            self.best_metric = 0.0
+        else:
+            self.best_metric = 10000.0
 
     def stop(self, current_metric):
         if self.mode == "max":
@@ -20,8 +23,25 @@ class Early_stopping():
                 self.current_patience -= 1
 
             print("patience left:{}, best({})".format(self.current_patience, self.best_metric))
+            print()
 
             if self.current_patience == 0:
                 return True
             else:
                 return False
+        else: # mode = "min"
+            if current_metric < self.best_metric:
+                self.best_metric = current_metric
+                self.current_patience = self.patience
+            else:
+                self.current_patience -= 1
+
+            print("patience left:{}, best({})".format(self.current_patience, self.best_metric))
+            print()
+
+            if self.current_patience == 0:
+                return True
+            else:
+                return False
+
+
