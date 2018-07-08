396bf43502e7ffb83916088a8e21b0dfd260fd7f
Katerina Margatina <katerina.margatina@gmail.com>
2018-07-08 22:05:31 +0300

    ensembling posteriors

--- submissions/ensembling.py
+++ submissions/ensembling.py
@@ -18,16 +18,36 @@ def f1_macro(y, y_hat):
 def f1_micro(y, y_hat):
     return f1_score(y, y_hat, average='micro')
 
-def ensemble_voting(predictions, gold):
+def ensemble_voting(predictions, gold, dataset):
     stacked = numpy.stack(predictions, axis=0)
     modals = stats.mode(stacked, axis=0)[0].squeeze().astype(int)
 
-    accuracy = acc(gold, modals)
-    f1 = f1_macro(gold, modals)
-    print("acc: ",accuracy)
-    print("f1: ",f1)
+    if dataset != "test":
+        accuracy = acc(gold, modals)
+        f1 = f1_macro(gold, modals)
+        print("acc: ",accuracy)
+        print("f1: ",f1)
+    else:
+        accuracy = 0
+        f1 = 0
 
-    return modals
+    return modals, accuracy, f1
+
+def ensemble_posteriors(posteriors, gold, dataset):
+
+    avg_posteriors = numpy.mean(numpy.stack(posteriors, axis=0), axis=0)
+    predictions = numpy.argmax(avg_posteriors, 1)
+
+    if dataset != "test":
+        accuracy = acc(gold, predictions)
+        f1 = f1_macro(gold, predictions)
+        print("acc: ",accuracy)
+        print("f1: ",f1)
+    else:
+        accuracy = 0
+        f1 = 0
+
+    return predictions, accuracy, f1
 
 def read_dev_gold():
     file = os.path.join(DATA_DIR, "wassa_2018", "trial-v3.labels")
--- submissions/submit.py
+++ submissions/submit.py
@@ -14,7 +14,7 @@ import config
 from config import DATA_DIR, BASE_PATH
 from model.params import WASSA_CONF
 from modules.neural.dataloading import WordDataset
-from submissions.ensembling import ensemble_voting, read_dev_gold
+from submissions.ensembling import ensemble_voting, read_dev_gold, ensemble_posteriors
 from utils.dataloaders import clean_text
 from utils.load_embeddings import load_word_vectors
 from utils.nlp import twitter_preprocessor
@@ -38,17 +38,18 @@ def load_test_wassa(dataset):
 
     _X = []
     _y = []
-    if dataset=='dev' or dataset=='train':
-        lines = open(file, "r", encoding="utf-8").readlines()
-        for line_id, line in enumerate(lines):
-            columns = line.rstrip().split("\t")
-            emotion = columns[0]
-            text = columns[1:]
-            text = clean_text(" ".join(text))
-            _X.append(text)
-            _y.append(emotion)
 
-    return _X, _y
+    lines = open(file, "r", encoding="utf-8").readlines()
+    for line_id, line in enumerate(lines):
+        columns = line.rstrip().split("\t")
+        emotion = columns[0]
+        text = columns[1:]
+        text = clean_text(" ".join(text))
+        _X.append(text)
+        _y.append(emotion)
+
+
+    return _X
 
 
 def process_batch_test_clf(model, batch, device):
@@ -92,7 +93,7 @@ def test_clf(model, data_source, device):
     return labels, predicted, posteriors
 
 def write_predictions(predictions, encoder):
-    fname = "predictions.txt"
+    fname = "predictions_val_13.txt"
     y_pred = [encoder.classes_[x] for x in predictions] # labels to strings
     with open(fname, "w") as outf:
         for prediction in y_pred:
@@ -102,7 +103,7 @@ def write_predictions(predictions, encoder):
 def submission(dataset, models=[], lm=[], gold=[]):
 
 
-    X, _ = load_test_wassa(dataset)
+    X = load_test_wassa(dataset)
 
 
     with open("label_encoder.pkl", "rb") as f:
@@ -136,7 +137,7 @@ def submission(dataset, models=[], lm=[], gold=[]):
         preprocessor = twitter_preprocessor()
 
         # for new experiments remember to empty _cache!
-        test_set = WordDataset(X, dummy_y, word2idx, name="wassa_test_submit"+str(i),
+        test_set = WordDataset(X, dummy_y, word2idx, name="wassa_test_submit",
                                preprocess=preprocessor)
         sampler = SequentialSampler(test_set)
 
@@ -155,26 +156,45 @@ def submission(dataset, models=[], lm=[], gold=[]):
         #####################################################################
         labels, predicted, posteriors = test_clf(model=model, data_source=test_loader,
                                                  device=config.DEVICE)
-        pprint(labels)
+        # pprint(labels)
         pprint(predicted)
 
         predicted_list.append(predicted)
         posteriors_list.append(posteriors)
 
-    pred = ensemble_voting(predicted_list, gold)
+    # pred, accuracy, f1  = ensemble_voting(predicted_list, gold, dataset)
+    pred, accuracy, f1 = ensemble_posteriors(posteriors_list, gold, dataset)
+
     #####################################################################
-    # Create submission file with the predictions
+    # Create submission file with the predictions3M_GU13__35_noconc_2att
     #####################################################################
-    # write_predictions(pred, label_encoder)
+    write_predictions(pred, label_encoder)
     return
 
 
 
 gold = read_dev_gold()
-models=["wassa2M_ep2_GU_18-06-30_20:32:31", "wassa_rnn_600_18-07-02_13:45:15", "wassa_2M_LM_FT_simple_noconc_18-07-02_12:54:22"]
-lm = [True, False, True]
+models=["wassa_rnn_350_18-07-02_16:30:49", #"wassa_rnn400_repr_18-07-03_20:08:37",
+        "wassa_sent250_18-07-06_14:30:42", #"wassa_sent400_18-07-06_14:41:00",
+        "5M__GU_35_noconc_18-07-05_14:59:25", #"5M__GU_69_conc_18-07-06_15:13:33",
+        "2M_LM_FT_GU_24__GU_57_18-07-08_01:02:16", #"wassa_2M_LM_FT_GU24_conc_GU45_18-07-03_17:53:31",
+        "LM_FT_simple__GU_5_7_18-07-07_17:29:54", #"step7_noquestions_18-07-03_01:05:37",
+        "2M_600_LM_FT_GU13__3_5_noconc_18-07-03_20:55:27",
+        "wassa_LM_FT_2M_GU23_conc_GU69_18-07-04_18:34:12", #"wassa_LM_FT_2M_GU23_noconc_GU1214_18-07-05_00:57:46", "wassa_LM_FT_2M_GU23_conc_GU36_18-07-04_02:26:56", "wassa_LM_FT_2M_GU23_noconc_GU69_18-07-05_00:31:39",
+        "3M_GU13__35_noconc_2att",
+        "TWITTER_3M_GU_24__45_conc_18-07-04_16:09:59"]
 
-submission(dataset="dev", models=models, lm=lm, gold=gold)
+lm = [False, #False,          # word2vec
+      False, #False,          # sentiment
+      True, #True,            # 5M simple
+      True, #True,            # 2M GU 2,4
+      True, #True,            # 2M simple
+      True,                  # 2M GU 1,3
+      True, #True, True, True,          # 2M GU 2,3
+      True,                  # 3M GU 1,3
+      True]                  # 3M Gu 2,4
+
+submission(dataset="trial-v3", models=models, lm=lm, gold=gold)
 
     # dataset = "train", "dev", or "test"
     # they should be stored in /datasets/wassa_2018
